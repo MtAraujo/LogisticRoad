05581bd92c4ae01bd355b8f119c79f22db5092fb
mtaraujo <matheus.t.araujo@hotmail.com>
2024-06-24 12:31:53 -0300

    primeira versao do jogo

--- Assets/Script/LoopingBackground.cs
+++ Assets/Script/LoopingBackground.cs
@@ -1,19 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LoopingBackground : MonoBehaviour
-{
-    public float BackgroundSpeed;
-    public Renderer BackgroundRenderer;
-
-    void Update()
-    {
-        BackgroundRenderer.material.mainTextureOffset += new Vector2(BackgroundSpeed*Time.deltaTime, 0f);
-    }
-}
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f; // Velocidade de movimento do jogador
+    public float speed = 5f; 
 
     void Update()
     {
-        // Captura o input do teclado
         float verticalInput = Input.GetAxis("Vertical");
 
-        // Calcula o vetor de movimento
         Vector3 movement = new Vector3( 0f, verticalInput) * speed * Time.deltaTime;
 
-        // Aplica o movimento ao jogador
         transform.Translate(movement);
     }
 }
